17352e0b9a2deb5ea0bc09b610d10ad205f04b9b
armaan48 <nougaiarmaan@gmail.com>
2023-06-25 21:21:42 +0530

    making signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,46 @@
-import './App.css';
-import {React , useState} from "react";
+import "./App.css";
+import { React, useState } from "react";
 import TopBar from "./windows/topBar/TopBar";
 import ChatArea from "./windows/chatArea/ChatArea";
 import LoginNSignup from "./windows/loginNSignup/LoginNSignup";
+import io from "socket.io-client";
+
+var socket = io.connect("http://localhost:3001", {
+    cors: {
+        origin: "*",
+    },
+});
 
 function App() {
-  const[chat , setChat] = useState(0);
-  return (
-    <div className='mainBody'>
-      {
-        chat
-        ?
-          <>
-            <TopBar />
-            <ChatArea />
-          </>
-        :
-          <LoginNSignup />
-      }
-      
-    </div>
-  );
+    const [enter, setEnter] = useState(0);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [privateKey, setPrivateKey] = useState("");
+    const [publicKey, setPublicKey] = useState("");
+
+    var curUserData = {
+        socketID: socket.id,
+        username,
+        setUsername,
+        password,
+        setPassword,
+        privateKey,
+        setPrivateKey,
+        publicKey,
+        setPublicKey,
+    };
+    return (
+        <div className="mainBody">
+            {enter ? (
+                <>
+                    <TopBar />
+                    <ChatArea />
+                </>
+            ) : (
+                <LoginNSignup socket={socket}  curUserData={curUserData} setEnter={setEnter} />
+            )}
+        </div>
+    );
 }
 
 export default App;
--- client/src/windows/loginNSignup/LoginNSignup.js
+++ client/src/windows/loginNSignup/LoginNSignup.js
@@ -1,39 +1,89 @@
-import React from "react";
-import './LoginNSignup.css';
+import {React , useEffect , useState} from 'react';
 
-const LoginNSignup = () => {
-    return(
-        <div className="loginNSignupMainBody"> 
+import "./LoginNSignup.css";
+import { generateRSAKeys, encrypt, encryptPrivateKey} from "../../helper.js";
+
+const LoginNSignup = (props) => {
+    const { socket, curUserData  ,setEnter } = props;
+    console.log(curUserData)
+
+    const enter = () => {
+        if (curUserData.password && curUserData.username) {
+            generateRSAKeys().then(({ publicKey, privateKey }) => {
+                curUserData.setPublicKey(publicKey);
+                curUserData.setPrivateKey(privateKey);
+                encryptPrivateKey(
+                    privateKey,
+                    curUserData.username + curUserData.password
+                ).then((encryptedPrivateKey) => {
+                    encrypt(publicKey, curUserData.password).then(
+                        (encryptedPassword) => {
+                            socket.emit("sign-up-init", {
+                                username: curUserData.username,
+                                publicKey,
+                                encryptedPrivateKey,
+                                encryptedPassword,
+                                socketID: socket.id,
+                            });
+                        }
+                    );
+                });
+            });
+        }
+    };
+
+
+    useEffect(()=>{
+        socket.on("sign-up-complete", data=>{
+            setEnter(1);
+        })
+
+        return ()=>{
+            socket.off("sign-up-complete");
+        }
+    },[socket])
+
+
+
+
+
+    return (
+        <div className="loginNSignupMainBody">
             <div className="mainBoxLoginArea">
-                <div className="welcomeBack">
-                    Welcome Back
-                </div>
+                <div className="welcomeBack">Welcome Back</div>
                 <div className="loginNSignupOption">
                     <div className="loginNsignupButtonArea">
                         <div className="slider"></div>
-                        <button className="loginButton">
-                            Log in
-                        </button>
-                        <button  className="signupButton">
-                            Sign up
-                        </button>
+                        <button className="loginButton">Log in</button>
+                        <button className="signupButton">Sign up</button>
                     </div>
                 </div>
                 <div className="userNameLoginPage">
-                    <input className="userNameInputBox" placeholder="Username"></input>
+                    <input
+                        className="userNameInputBox"
+                        placeholder="Username"
+                        onChange={(e) => {
+                            curUserData.setUsername(e.target.value);
+                        }}
+                    ></input>
                 </div>
                 <div className="passwordLoginPage">
-                    <input className="passwordInputBox" placeholder="Password"></input>
+                    <input
+                        className="passwordInputBox"
+                        placeholder="Password"
+                        onChange={(e) => {
+                            curUserData.setPassword(e.target.value);
+                        }}
+                    ></input>
                 </div>
                 <div className="enterLoginPage">
-                    <button  className="enterButton">
+                    <button className="enterButton" onClick={enter}>
                         Enter
                     </button>
                 </div>
-
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default LoginNSignup;
